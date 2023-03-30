3f6df4164c2369e9f3ae2b78cca10eb254bf4543
brandonruizmora <brandon_ruizmor@outlook.es>
2023-03-30 11:58:59 -0600

    bases

---
+++ bases/app.js
@@ -0,0 +1,9 @@
+console.clear();
+const base = 5;
+console.log("==================");
+console.log("Tabla del " + 5);
+console.log("==================");
+
+for (let i = 1; i <= base*2; i++) {
+    console.log(`${base} X ${i} = ${base*i}`);
+}
