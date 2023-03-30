593828179bc492b69d72eed3f7001944e9b918a2
brandonruizmora <brandon_ruizmor@outlook.es>
2023-03-30 12:21:22 -0600

    using filesystem node module

--- bases/app.js
+++ bases/app.js
@@ -1,9 +1,21 @@
+const fs = require('node:fs');
+
 console.clear();
-const base = 5;
+
+const base = 3;
+let salida = "";
+
 console.log("==================");
 console.log("Tabla del " + 5);
 console.log("==================");
 
-for (let i = 1; i <= base*2; i++) {
-    console.log(`${base} X ${i} = ${base*i}`);
+for (let i = 1; i <= 10; i++) {
+    salida += `${base} X ${i} = ${base*i}\n`;
 }
+
+console.log(salida);
+
+fs.writeFile(`Tabla-${base}.txt`, salida, (err) => {
+  if (err) throw err;
+  console.log(`Se guardo el archivo Tabla-${base}.txt`);
+});
